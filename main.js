b6087afcf8b3eff5ab6b28ba5fc7512045c228f3
fallOut015 <captainDoodles264@gmail.com>
2020-06-20 22:31:52 -0700

    bold

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ Game.Win('Third-party')
 
 //Game.customChecks.push(new Game.Upgrade("name", "desc", 1, [0, 0]))
 //Game.last.unlock()
-Game.customChecks.push(Game.NewUpgradeCookie({ name : "Thumbprint Cookies", desc : "Also known as Hallengrotta. You've Hallen<i>gotta</i> try 'em! <br/> Get it?", icon : [2, 3, "https://fallout015.github.io/icons.png"], power : 2, price : 1 }))
+Game.customChecks.push(Game.NewUpgradeCookie({ name : "Thumbprint cookies", desc : "Also known as Hallengrotta. You've Hallen<b>gotta</b> try 'em! <br/> Get it?", icon : [2, 3, "https://fallout015.github.io/icons.png"], power : 2, price : 1 }))
 Game.last.unlock()
