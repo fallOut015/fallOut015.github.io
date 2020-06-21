65a85899474a7c1e1e23dea26111694a46042d26
fallOut015 <captainDoodles264@gmail.com>
2020-06-20 21:52:55 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 JarOfCookies = {};
 
 window.onload = event => {
-	Game.NewUpgradeCookie({ name : 'name', desc : 'desc', icon : [4,4], power : 2, price : 1 });
+	alert(event)
+	Game.NewUpgradeCookie({ name : 'name', desc : 'desc', icon : [4,4], power : 2, price : 1 })
 	new Game.Upgrade("name", "desc", 1, [0, 0])
 }
