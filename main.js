feec57f6e6853c5884163b4b7fbb769260bb7260
fallOut015 <captainDoodles264@gmail.com>
2020-06-20 22:06:09 -0700

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ Game.Win('Third-party')
 
 Game.customChecks.push(new Game.Upgrade("name", "desc", 1, [0, 0]))
 Game.last.unlock()
+Game.customChecks.push(Game.NewUpgradeCookie({ name : 'name', desc : 'desc', icon : [4,4], power : 2, price : 1 }))
+Game.last.unlock()
