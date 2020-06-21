2915ee1e8f242a19de23063d7c94c5feb528f0b7
fallOut015 <captainDoodles264@gmail.com>
2020-06-20 22:17:33 -0700

    updated icons

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ Game.Win('Third-party')
 
 Game.customChecks.push(new Game.Upgrade("name", "desc", 1, [0, 0]))
 Game.last.unlock()
-Game.customChecks.push(Game.NewUpgradeCookie({ name : 'name', desc : 'desc', icon : [4,4], power : 2, price : 1 }))
+Game.customChecks.push(Game.NewUpgradeCookie({ name : 'name', desc : 'desc', icon : [2, 4, "https://fallout015.github.io/icons.png"], power : 2, price : 1 }))
 Game.last.unlock()
