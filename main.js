14cb23f7b7f3523168c858902bf72da207b95cd9
fallOut015 <captainDoodles264@gmail.com>
2020-06-20 22:21:35 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ alert("Loading Jar of Cookies")
 
 Game.Win('Third-party')
 
-Game.customChecks.push(new Game.Upgrade("name", "desc", 1, [0, 0]))
-Game.last.unlock()
+//Game.customChecks.push(new Game.Upgrade("name", "desc", 1, [0, 0]))
+//Game.last.unlock()
 Game.customChecks.push(Game.NewUpgradeCookie({ name : "Thumbprint Cookies", desc : "Also known as Hallengrotta. You've Hallen<i>gotta</i> try 'em. <br/> Get it?", icon : [2, 3, "https://fallout015.github.io/icons.png"], power : 2, price : 1 }))
 Game.last.unlock()
