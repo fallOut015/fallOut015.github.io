9d6b96943278e6db212c21aa18f165c39d7266f7
fallOut015 <captainDoodles264@gmail.com>
2020-06-22 13:25:29 -0700

    Update main.js

--- main.js
+++ main.js
@@ -34,7 +34,11 @@ alert("Loading Jar of Cookies")
 
 Game.Win("Third-party")
 
+var getCookiePrice=function(level){return 999999999999999999999999999999999999999*Math.pow(10,(level-1)/2);};
+
 Game.NewUpgradeCookie({ name : "Thumbprint cookies", desc : "Also known as Hallengrotta. You've Hallen<b>gotta</b> try 'em! <br/> Get it?", icon : [2, 3, icons], power : 4, price : 99999999999999999999*5 })
 Game.NewUpgradeCookie({ name : "Lava cookies", desc : "Hopefully not real lava.<br/>Right?", icon : [1, 3, icons], power : 2, price : 999999999999*5 })
 Game.NewUpgradeCookie({ name : "Those Pillsbury cookies with the christmas tree", desc : "I hope I'm not the only one with a childlike fondness for these. They have a certain... texture.", icon : [3, 3, icons], power : 4, price : 999999999999999999999999999999 })
 Game.NewUpgradeCookie({ name : "Donut cookies", desc : "You're not sure how to feel about these. Something about it feels wrong.<br/>On the other hand, it's two treats in one! Best of both worlds!", icon : [1, 4, icons], power : 4, price : 9999999999999999999999999999999999 })
+Game.NewUpgradeCookie({ name : "Brookies", desc : "It's like a soft brownie and a delicious cookie had a baby. I can feel the calories writing this.", icon : [2, 4, icons], power : 5, price : getCookiePrice(8) })
+Game.NewUpgradeCookie({ name : "Crownies", desc : "Wait, these actually exist?", icon : [3, 4, icons], power : 1, price : getCookiePrice(11) })
