c55a1225e78d5869e0f8f4e83c2033ba8d57a263
fallOut015 <captainDoodles264@gmail.com>
2021-05-17 13:20:06 -0700

    stuff

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-window.onload = event => {
+"use strict";
 
+function toggleClass(element, className) {
+    if(element.classList.contains(className)) {
+        element.classList.remove(className);
+    } else {
+        element.classList.add(className);
+    }
+}
+
+window.onload = event => {
+    const infos = document.getElementsByClassName("info");
+    for(let i = 0; i < infos.length; ++ i) {
+        const info = infos[i];
+        info.onclick = event => {
+            const mod = document.getElementsByClassName("mod")[i];
+            toggleClass(mod, "expanded");
+        }
+    }
 }
